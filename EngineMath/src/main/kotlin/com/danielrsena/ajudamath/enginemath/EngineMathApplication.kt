1327ff82f89fb254d03f03317858820966d5e0aa
Daniel Ramos Sena <danielsena20@hotmail.com>
2025-04-24 23:54:23 -0300

    Adição do EngineMath
    O EngineMath será o componente de back-end do SiteMath, fornecendo APIs REST para funções matemáticas e no futuro terá integração com o ExerciMath.

---
+++ EngineMath/src/main/kotlin/com/danielrsena/ajudamath/enginemath/EngineMathApplication.kt
@@ -0,0 +1,11 @@
+package com.danielrsena.ajudamath.enginemath
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EngineMathApplication
+
+fun main(args: Array<String>) {
+	runApplication<EngineMathApplication>(*args)
+}
---
+++ EngineMath/src/test/kotlin/com/danielrsena/ajudamath/enginemath/EngineMathApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.danielrsena.ajudamath.enginemath
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EngineMathApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
